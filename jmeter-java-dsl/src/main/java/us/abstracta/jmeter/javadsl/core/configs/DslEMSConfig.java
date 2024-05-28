f1264d3eb79203f15b0f2bc81767e682fd89774f
Huang <xiaobing.huang@icloud.com>
2024-05-28 10:46:00 +0800

    support packaging ems-jmeter-plugin

--- jmeter-java-dsl/src/main/java/us/abstracta/jmeter/javadsl/core/configs/DslEMSConfig.java
+++ jmeter-java-dsl/src/main/java/us/abstracta/jmeter/javadsl/core/configs/DslEMSConfig.java
@@ -6,34 +6,32 @@ import org.apache.jmeter.testelement.TestElement;
 import us.abstracta.jmeter.javadsl.codegeneration.MethodCall;
 import us.abstracta.jmeter.javadsl.codegeneration.MethodCallBuilder;
 import us.abstracta.jmeter.javadsl.codegeneration.MethodCallContext;
-
 import java.lang.reflect.Method;
 import java.util.List;
 
 public class DslEMSConfig extends BaseConfigElement {
-   public DslEMSConfig() {
-        super("EMSConfig", TestBeanGUI.class);
-   }
+  public DslEMSConfig() {
+    super("EMSConfig", TestBeanGUI.class);
+  }
 
-   protected TestElement buildTestElement() {
-      return new EMSConfig();
-   }
+  protected TestElement buildTestElement() {
+    return new EMSConfig();
+  }
 
-   public static class CodeBuilder extends MethodCallBuilder {
-      public CodeBuilder(List<Method> builderMethods) {
-         super(builderMethods);
-      }
+  public static class CodeBuilder extends MethodCallBuilder {
+    public CodeBuilder(List<Method> builderMethods) {
+      super(builderMethods);
+    }
 
-       @Override
-       public boolean matches(MethodCallContext context) {
-           TestElement testElement = context.getTestElement();
-           return testElement.getClass() == EMSConfig.class;
-       }
+    @Override
+    public boolean matches(MethodCallContext context) {
+      TestElement testElement = context.getTestElement();
+      return testElement.getClass() == EMSConfig.class;
+    }
 
-       @Override
-       protected MethodCall buildMethodCall(MethodCallContext context) {
-           TestElement testElement = context.getTestElement();
-           return buildMethodCall();
-       }
-   }
+    @Override
+    protected MethodCall buildMethodCall(MethodCallContext context) {
+      return buildMethodCall();
+    }
+  }
 }
--- jmeter-java-dsl/src/main/java/us/abstracta/jmeter/javadsl/core/configs/DslEMSIntegrationToken.java
+++ jmeter-java-dsl/src/main/java/us/abstracta/jmeter/javadsl/core/configs/DslEMSIntegrationToken.java
@@ -1,6 +1,5 @@
 package us.abstracta.jmeter.javadsl.core.configs;
 
-import com.sap.csc.ems.config.EMSConfig;
 import com.sap.csc.ems.integration.token.EMSIntegrationToken;
 import org.apache.jmeter.testbeans.gui.TestBeanGUI;
 import org.apache.jmeter.testelement.TestElement;
@@ -12,29 +11,29 @@ import java.lang.reflect.Method;
 import java.util.List;
 
 public class DslEMSIntegrationToken extends BaseConfigElement {
-   public DslEMSIntegrationToken() {
-        super("EMSIntegrationToken", TestBeanGUI.class);
-   }
+  public DslEMSIntegrationToken() {
+    super("EMSIntegrationToken", TestBeanGUI.class);
+  }
 
-   protected TestElement buildTestElement() {
-      return new EMSIntegrationToken();
-   }
+  protected TestElement buildTestElement() {
+    return new EMSIntegrationToken();
+  }
 
-   public static class CodeBuilder extends MethodCallBuilder {
-      public CodeBuilder(List<Method> builderMethods) {
-         super(builderMethods);
-      }
+  public static class CodeBuilder extends MethodCallBuilder {
+    public CodeBuilder(List<Method> builderMethods) {
+      super(builderMethods);
+    }
 
-       @Override
-       public boolean matches(MethodCallContext context) {
-           TestElement testElement = context.getTestElement();
-           return testElement.getClass() == EMSIntegrationToken.class;
-       }
+    @Override
+    public boolean matches(MethodCallContext context) {
+      TestElement testElement = context.getTestElement();
+      return testElement.getClass() == EMSIntegrationToken.class;
+    }
 
-       @Override
-       protected MethodCall buildMethodCall(MethodCallContext context) {
-           TestElement testElement = context.getTestElement();
-           return buildMethodCall();
-       }
-   }
+    @Override
+    protected MethodCall buildMethodCall(MethodCallContext context) {
+      TestElement testElement = context.getTestElement();
+      return buildMethodCall();
+    }
+  }
 }
